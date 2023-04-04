f84f3376a1caca217aa80fc4e5136d11e70ec2c8
rajpopzein <kraj0132@gmail.com>
2023-04-04 14:48:33 +0530

    changed delete module

--- controller/users.js
+++ controller/users.js
@@ -3,32 +3,24 @@ import {MongoClient} from 'mongodb';
 import {ServerApiVersion} from 'mongodb'
 
 
-
-
 const uri = "mongodb+srv://rajkumarr:popzein_helen_123@cluster0.lrnwsdq.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri)
-
-
 const database = client.db("testing");
 const test = database.collection("test");
 
-const createuser = async(data) =>{
+const createuser = async(data) => {
     try{
       const doc = data
       const result =await test.insertOne(doc)  
       console.log(`document was inserted ${result}`)
-  
-    }finally{
-      await client.close();
     }
-  }
-
-
-
-
+    catch(e){
+        console.log('eoor',e)
+    }
+    
+}
 
 let userss =[]
-
 
 export const getAllUser = async(req,res) => {
     const client = new MongoClient(uri)
@@ -36,19 +28,16 @@ export const getAllUser = async(req,res) => {
     const alldata = await test.find({}).toArray()
     // console.log(alldata)
     res.status(200).json({status:'ok', data:alldata})
-    
     }
     catch(e){
-        console.log("error fetching data",e)
+        console.log("Error fetching data",e)
     }
     finally{
         await client.close();
     }
 }
 
-
-
-export const createUser = async(req,res)=>{
+export const createUser = async(req,res) => {
     const client = new MongoClient(uri)
     const user = req.body;
     // const userId = uuidv4();
@@ -56,30 +45,25 @@ export const createUser = async(req,res)=>{
     const id = uuidv4();
     const userss =(await test.find({}).toArray());
     const existingDataCheck = userss.find((users)=> users.name == user.name);
-    console.log(existingDataCheck);
+    // console.log(existingDataCheck);
     if(existingDataCheck == undefined)
     {
         const userWithId = {...user, id};
-
-// ////////////////////////////////////////////////////////////////////////////////
-        createuser(userWithId);
-// ////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+        await createuser(userWithId);
+//////////////////////////////////////////////////////////////////////////////////////
         res.status(201).json({status:'ok'});
     }
     else{
         res.status(403).json({status:'user already exist'});
     }
-   
 }
 
-export const getUserById = async (req,res)=>{
+export const getUserById = async (req,res) => {
     const {id} = req.params;
     const client = new MongoClient(uri)
-   
     const userData = await userss.find((user) => user.id == id);
-
     // res.status(200).json({status:'Success', data:userData})
-
     if(userData == undefined){
         res.status(404).json({status:"User not found"});
     }
@@ -88,27 +72,30 @@ export const getUserById = async (req,res)=>{
     }
 }
 
-
-export const deleteUserById = async(req,res)=>{
+export const deleteUserById = async(req,res) => {
     const {id} = req.params;
     const client = new MongoClient(uri)
     const alldata = await test.find({}).toArray();
-    const deletedId = userss.filter((user)=> user.id != id);
-
-    for (let data in alldata){
-        console.log('>>>>>>>',data)
+    const deletedId = alldata.filter((user)=> user.id == id);
+   
+    if(deletedId.length > 0)
+    { 
+        test.deleteOne(deletedId[0],(err,obj)=>{
+            if(err) throw err
+            database.close()
+        })
+        res.status(200).json({status:'Success'});
     }
-    res.status(200).json({status:'Success'});
+    else{
+        res.status(200).json({status:'Data not found'});
+    }
 }
 
-export const updateUser = (req,res)=>{
+export const updateUser = (req,res) => {
     const {id} = req.params;
     const client = new MongoClient(uri)
-
     const user = userss.filter((user)=>user.id == id)
-
     const {name,age,address} = req.body;
-
     if(name){
         user.name = name
     }
